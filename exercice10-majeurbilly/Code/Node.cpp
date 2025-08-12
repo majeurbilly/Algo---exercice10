83f829fc5ad8fc339b6ddee643807d5de0a8b018
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2025-08-12 08:05:07 -0400

    start

---
+++ exercice10-majeurbilly/Code/Node.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include "Node.h"
+
+Node::Node()
+	: next(nullptr)
+{
+}
+
+
+Node::~Node()
+{
+}
+
+Node* Node::getNext() const
+{
+	return this->next;
+}
+
+void Node::setNext(Node* _next)
+{
+	this->next = _next;
+}
+
+const Content&  Node::getContent() const
+{
+	return this->content;
+}
+
+Content& Node::getContent() 
+{
+	return this->content;
+}
+
+void Node::setContent(const Content& _content)
+{
+	this->content = _content;
+}
+
+
---
+++ exercice10-majeurbilly/TestsCode/teststack.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Stack.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestsCode
+{
+  TEST_CLASS(TestsStack){
+    public :
+
+        TEST_METHOD(Constructor){
+            /*
+              // TODO: Ici, votre code de test
+              Stack stack;
+
+              Assert::IsTrue(stack.isEmpty());
+              Assert::AreEqual((unsigned int)0, stack.size());
+
+            */
+        }
+    // Compl�tez les tests ici
+    // Exemples de tests � produire:
+    // Tester un push sur pile vide (v�rifiez la taille, si la pile est vide, le contenu du top)
+    //  ...
+
+    // Plus difficile car syntaxe diff�rente avec les exceptions
+    // Tester un pop d'une pile vide (exception)
+    // ...
+
+    // Vous devez impl�menter tous les tests visant � mettre � l'�preuve votre structure.
+    // Vous pouvez utiliser la technique de votre choix pour la génération des tests.
+  };
+}
