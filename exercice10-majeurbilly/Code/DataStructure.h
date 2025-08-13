b187e35e39e49b39f820093ef1e123fe5d7fd500
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2025-08-13 14:21:56 -0400

    finish

--- exercice10-majeurbilly/Code/DataStructure.h
+++ exercice10-majeurbilly/Code/DataStructure.h
@@ -8,23 +8,23 @@ namespace TestsCode
 }
 class DataStructure
 {
-  public:
-    DataStructure();
-    DataStructure(const DataStructure& src);
-    virtual ~DataStructure();  // destructeur virtual permet de delete sur d'autre objet potentiel
-    virtual void display() const = 0;
-    unsigned int size() const;
-    virtual bool isEmpty() const;
-  protected:
-    const Node* getFirstNode() const;
-    Node* getFirstNode();
-    void setFirstNode(Node* node);
+public:
+  DataStructure();
+  DataStructure(const DataStructure& src);
+  virtual ~DataStructure();  // destructeur virtual permet de delete sur d'autre objet potentiel
+  virtual void display() const = 0;
+  unsigned int size() const;
+  virtual bool isEmpty() const;
+protected:
+  const Node* getFirstNode() const;
+  Node* getFirstNode();
+  void setFirstNode(Node* node);
 
-  private:
-    Node* firstNode; 
+private:
+  Node* firstNode; 
 
-    // Laissez ce code ici
-    friend TestsCode::TestsQueue;
-    friend TestsCode::TestsStack;
+  // Laissez ce code ici
+  friend TestsCode::TestsQueue;
+  friend TestsCode::TestsStack;
 };
 
--- exercice10-majeurbilly/Code/Stack.h
+++ exercice10-majeurbilly/Code/Stack.h
@@ -7,13 +7,13 @@ using namespace std;
 
 class Stack : public DataStructure
 {
-	public:
-		Stack();
-		Stack(const Stack& src);
-		~Stack();
-		const Content& top() const;
-		void push(const Content& content);
-		void pop();
-		void display() const;
+public:
+	Stack();
+	Stack(const Stack& src);
+	~Stack();
+	const Content& top() const;
+	void push(const Content& content);
+	void pop();
+	void display() const;
 };
 
